95467064810630c1298a5377e470b7c5e922bfb9
manipushpak <manipusg@usc.edu>
2018-09-05 11:41:38 -0700

    added files

---
+++ Hamster/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Hamster
+//
+//  Created by Teresa Liu on 11/12/16.
+//  Copyright © 2016 Teresa Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
